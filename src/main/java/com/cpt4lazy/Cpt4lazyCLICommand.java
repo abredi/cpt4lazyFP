145b8dac27c7a9d10605fce38f71cd0515d4fef6
AmanuelChorito <amye.end@gmail.com>
2021-04-13 15:24:33 -0500

    new pipelline added to functional util

--- src/main/java/com/cpt4lazy/Cpt4lazyCLICommand.java
+++ src/main/java/com/cpt4lazy/Cpt4lazyCLICommand.java
@@ -3,13 +3,14 @@ package com.cpt4lazy;
 import com.cpt4lazy.commands.AlumniWithMostRequestAccepted;
 import com.cpt4lazy.commands.AlumniWithMostRequestRejected;
 import com.cpt4lazy.commands.TopKCommonSkills;
+import com.cpt4lazy.commands.TopKJobsApplied;
 import io.micronaut.configuration.picocli.PicocliRunner;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 
 @Command(name = "cpt4lazyCLI", description = "...",
-        mixinStandardHelpOptions = true, subcommands = {AlumniWithMostRequestAccepted.class, AlumniWithMostRequestRejected.class, TopKCommonSkills.class})
+        mixinStandardHelpOptions = true, subcommands = {AlumniWithMostRequestAccepted.class, AlumniWithMostRequestRejected.class, TopKCommonSkills.class, TopKJobsApplied.class})
 public class Cpt4lazyCLICommand implements Runnable {
 
     @Option(names = {"-v", "--verbose"}, description = "...")
